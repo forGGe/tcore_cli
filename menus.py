d8e4911c7873ff84bc6b75eb689bceb9f4f8f6b2
Max Payne <forgge@gmail.com>
2018-05-08 00:50:32 +0300

    new engine

--- menus.py
+++ menus.py
@@ -5,6 +5,7 @@ import json
 import npyscreen, curses
 import re
 import sys
+import abc
 
 params_json=json.loads('''{
     "menu-platform": {
@@ -24,7 +25,22 @@ params_json=json.loads('''{
             "config-device": {
                 "type": "enum",
                 "values": ["STM32F4", "STM32L4"],
-                "description": "Desired device"
+                "description": "Desired device1"
+            },
+            "config-device2": {
+                "type": "enum",
+                "values": ["STM32F4", "STM32L4"],
+                "description": "Desired device2"
+            },
+            "config-device3": {
+                "type": "enum",
+                "values": ["STM32F4", "STM32L4"],
+                "description": "Desired device3"
+            },
+            "config-device44": {
+                "type": "enum",
+                "values": ["STM32F4", "STM32L4"],
+                "description": "Desired device4"
             },
 
             "config-clock": {
@@ -35,7 +51,7 @@ params_json=json.loads('''{
             },
 
             "config-uart": {
-                "type": "array",
+                "type": "table",
                 "description": "UART configuration",
                 "key": "config-channel",
                 "items": {
@@ -207,81 +223,335 @@ class TestApp2(npyscreen.NPSAppManaged):
     def onCleanExit(self):
         npyscreen.notify_wait("Goodbye!")
 
+#-------------------------------------------------------------------------------
+
+class abstract_ui(abc.ABC):
+    @abc.abstractmethod
+    def set_engine(self, engine):
+        pass
+
+    @abc.abstractmethod
+    def create_menu(self, menu_id):
+        pass
+
+    @abc.abstractmethod
+    def delete_menu(self, menu_id):
+        pass
+
+    @abc.abstractmethod
+    def create_config(self, menu_id, id, type, description, long_description=None, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def delete_config(self, menu_id, id):
+        pass
 
 
-# class MyTestApp(npyscreen.NPSAppManaged):
-#     def onStart(self):
-#         # When Application starts, set up the Forms that will be used.
-#         # These two forms are persistent between each edit.
-#         self.addForm("MAIN",       MainForm, name="Screen 1", color="IMPORTANT",)
-#         self.addForm("SECOND",     MainForm, name="Screen 2", color="WARNING",  )
-#         # This one will be re-created each time it is edited.
-#         self.addFormClass("THIRD", MainForm, name="Screen 3", color="CRITICAL",)
+class engine:
+    def __init__(self, ui_instance):
+        self.ui_instance = ui_instance
+        self.config_data = {}
+        self.config_params = params_json
+        self.output_cfg = cfg_json
 
-#     def onCleanExit(self):
-#         npyscreen.notify_wait("Goodbye!")
+        # Root form must be named as 'MAIN'
+        root_menu_id = 'MAIN'
+        self.ui_instance.set_engine(self)
+        self.ui_instance.create_menu(None, root_menu_id, 'Welcome to theCore configurator')
+        self.populate_menu(None, root_menu_id, self.config_params, self.output_cfg)
 
-#     def change_form(self, name):
-#         # Switch forms.  NB. Do *not* call the .edit() method directly (which
-#         # would lead to a memory leak and ultimately a recursion error).
-#         # Instead, use the method .switchForm to change forms.
-#         self.switchForm(name)
+    def on_config_change(self, menu_id, id, **kwargs):
 
-#         # By default the application keeps track of every form visited.
-#         # There's no harm in this, but we don't need it so:
-#         self.resetHistory()
+        return
 
-# class MainForm(npyscreen.ActionForm):
-#     def create(self):
-#         self.add(npyscreen.TitleText, name="Text:", value= "Press ^T to change screens" )
+    def populate_menu(self, p_menu_id, menu_id, menu_params, output_obj):
+        for k, v in menu_params.items():
+            if k.startswith('config-'):
+                # Initialize empty config, later UI will publish changes to it
+                output_obj[k] = {}
+                self.config_data[k] = { 'data': v, 'menu': menu_id, 'out': output_obj }
 
-#         self.add_handlers({"^T": self.change_forms})
+        for k, v in menu_params.items():
+            if k.startswith('config-'):
+                type = v['type']
 
-#     def on_ok(self):
-#         # Exit the application if the OK button is pressed.
-#         self.parentApp.switchForm(None)
+                if type == 'enum':
+                    self.ui_instance.create_config(menu_id, k,
+                        'enum', description=v['description'], values=v['values'])
+                elif type == 'integer':
+                    self.ui_instance.create_config(menu_id, k,
+                        'integer', description=v['description'])
 
-#     def change_forms(self, *args, **keywords):
-#         if self.name == "Screen 1":
-#             change_to="SECOND"
-#         elif self.name == "Screen 2":
-#             change_to="THIRD"
-#         else:
-#             change_to="MAIN"
+            elif k.startswith('menu-'):
+                new_menu_id = menu_id + '/' + k
+                self.ui_instance.create_menu(menu_id, new_menu_id,
+                    description=v['description'])
 
-#         # Tell the MyTestApp object to change forms.
-#         self.parentApp.change_form(change_to)
+                output_obj[k] = {}
+                self.populate_menu(menu_id, new_menu_id, v, output_obj[k])
 
-class TestApp(npyscreen.NPSApp):
-    def main(self):
+#-------------------------------------------------------------------------------
 
+# Navigation widget, button that leads to the given form
+class switch_form_btn(npyscreen.MiniButtonPress):
+    def __init__(self, *args, **kwargs):
+        self.target_form=kwargs['target_form']
+        super().__init__(*args, **kwargs)
+
+    def whenPressed(self):
+        self.parent.parentApp.switchForm(self.target_form)
+
+class npyscreen_multiline(npyscreen.MultiLineAction):
+    def __init__(self, *args, **kwargs):
+        self.ui=kwargs['ui']
+        self.f_id=kwargs['f_id']
+        super().__init__(*args, **kwargs)
+
+    def actionHighlighted(self, act_on_this, key_press):
+        self.ui.on_item_selected(self.f_id, act_on_this)
+
+class npyscreen_form(npyscreen.ActionFormV2WithMenus):
+    def __init__(self, *args, **kwargs):
+        self.my_f_id = kwargs['my_f_id']
+        self.ui = kwargs['ui']
+
+        super().__init__(*args, **kwargs)
+
+    def create(self):
+        pass
+
+    def adjust_widgets(self, *args, **keywords):
+        self.ui.check_widgets(self.my_f_id)
+
+class npyscreen_ui(abstract_ui):
+    def __init__(self, npyscreen_app):
+        self.menu_forms = {}
+        self.npyscreen_app = npyscreen_app
+        self.engine = None
+
+    def set_engine(self, engine):
+        self.engine = engine
+
+    def create_menu(self, p_menu_id, menu_id, description, long_description=None):
+        f = self.npyscreen_app.addForm(menu_id, npyscreen_form,
+            name=description, my_f_id=menu_id, ui=self)
+
+        if p_menu_id:
+            # Add navigation between forms
+            self.menu_forms[p_menu_id]['form'].add(switch_form_btn,
+                name='>>> Go to {}'.format(menu_id), target_form=menu_id)
+
+            f.add(switch_form_btn,
+                name='<<< Back to {}'.format(p_menu_id), target_form=p_menu_id)
+
+        ms = f.add(npyscreen.OptionListDisplay, name="Option List",
+                values = npyscreen.OptionList().options,
+                scroll_exit=True,
+                max_height=5)
+
+        self.menu_forms[menu_id] = { 'parent': p_menu_id, 'form': f, 'config_widget': ms }
+        # Empty configuration dict, will be populated in create_config() function
+        self.menu_forms[menu_id]['config_fields'] = {}
+
+    def delete_menu(self, menu_id):
+        pass
+
+    def create_config(self, menu_id, id, type, description, long_description=None, **kwargs):
+        self.menu_forms[menu_id]['config_fields'][id] = {
+            'form': menu_id,
+            'type': type,
+            'description': description,
+            'long_description': long_description,
+            'last_value': ''
+        }
+
+        if type == 'enum':
+            self.menu_forms[menu_id]['config_fields'][id]['option'] = \
+                npyscreen.OptionSingleChoice(description, choices=kwargs['values'])
+        else:
+            self.menu_forms[menu_id]['config_fields'][id]['option'] = \
+                npyscreen.OptionFreeText(description)
+
+        self.update_form(menu_id)
+
+    def delete_config(self, menu_id, id):
+        self.menu_forms[menu_id]['config_fields'].pop(id, None)
+        pass
+
+    ''' Private method, updates form '''
+    def update_form(self, f_id):
+        Options = npyscreen.OptionList()
+        options = Options.options
+
+        for id, data in self.menu_forms[f_id]['config_fields'].items():
+            options.append(data['option'])
+
+        self.menu_forms[f_id]['config_widget'].values = options
+
+        # This method is heavy, but redraws entire screen without glitching
+        # option list itself (as .display() does)
+        self.menu_forms[f_id]['form'].DISPLAY()
+
+    ''' Private method, checks if there are any updates on form widgets '''
+    def check_widgets(self, f_id):
+        fields = self.menu_forms[f_id]['config_fields']
+
+        for id, data in fields.items():
+            if data['last_value'] != data['option'].value:
+                data['last_value'] = data['option'].value
+                # Report one config at a time
+                self.engine.on_config_change(f_id, id, value=data['option'].value)
+                return
+
+        self.update_form(f_id)
+
+    ''' Private method, catches event from user '''
+    def on_item_selected(self, f_id, act_on_this):
+        fields = self.menu_forms[f_id]['config_fields']
+
+        field_id = None
+
+        for id, data in fields.items():
+            if data['description'] == act_on_this:
+                field_id = id
+
+        npyscreen.notify_ok_cancel('Hello ', title="Message", form_color='STANDOUT')
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+class theCoreConfiguratorApp(npyscreen.NPSAppManaged):
+    def onStart(self):
+        self.ui = npyscreen_ui(self)
+        self.engine = engine(self.ui)
+
+#-------------------------------------------------------------------------------
+
+
+class MyTestApp(npyscreen.NPSAppManaged):
+    def onStart(self):
+        self.registerForm("MAIN", MyMainForm())
+
+class MyMainForm(npyscreen.FormWithMenus):
+    def create(self):
+        self.add(npyscreen.TitleText, name = "Text:", value= "Just some text." )
+        self.how_exited_handers[npyscreen.wgwidget.EXITED_ESCAPE]  = self.exit_application
+
+        # The menus are created here.
+        self.m1 = self.add_menu(name="Main Menu", shortcut="^M")
+        self.m1.addItemsFromList([
+            ("Display Text", self.whenDisplayText, None, None, ("some text",)),
+            ("Just Beep",   self.whenJustBeep, "e"),
+            ("Exit Application", self.exit_application, "é"),
+        ])
+
+        self.m2 = self.add_menu(name="Another Menu", shortcut="b",)
+        self.m2.addItemsFromList([
+            ("Just Beep",   self.whenJustBeep),
+        ])
+
+        self.m3 = self.m2.addNewSubmenu("A sub menu", "^F")
+        self.m3.addItemsFromList([
+            ("Just Beep",   self.whenJustBeep),
+        ])
+
+    def whenDisplayText(self, argument):
+       npyscreen.notify_confirm(argument)
+
+    def whenJustBeep(self):
+        curses.beep()
+
+    def exit_application(self):
+        curses.beep()
+        self.parentApp.setNextForm(None)
+        self.editing = False
+        self.parentApp.switchFormNow()
+
+class MyMulti(npyscreen.MultiLineAction):
+    def actionHighlighted(self, act_on_this, key_press):
+        self.parent.ch = act_on_this
+
+class MyMainFormTest(npyscreen.FormWithMenus):
+    def create(self):
+        self.ch = '<<<>>>'
+        self.f = self.add(npyscreen.TitleFixedText, name = "Fixed Text:" , value="This is fixed text")
+        t = self.add(npyscreen.TitleText, name = "Text:", )
+        p = self.add(npyscreen.TitlePassword, name = "Password:")
+        fn = self.add(npyscreen.TitleFilename, name = "Filename:")
+        dt = self.add(npyscreen.TitleDateCombo, name = "Date:")
+        cb = self.add(npyscreen.Checkbox, name = "A Checkbox")
+        s = self.add(npyscreen.TitleSlider, out_of=12, name = "Slider")
+        self.ml= self.add(MyMulti,
+            values = [ 'try typing here!', 'Mutiline text, press ^R to reformat',  'nPress ^X for automatically created list of menus' ],
+            max_height=5, rely=9)
+        ms= self.add(npyscreen.TitleSelectOne, max_height=4, value = [1,], name="Pick One",
+                values = ["Option1","Option2","Option3"], scroll_exit=True, width=30)
+        ms2= self.add(npyscreen.MultiSelect, max_height=4, value = [1,],
+                values = ["Option1","Option2","Option3"], scroll_exit=True, width=20)
+
+        bn = self.add(npyscreen.MiniButton, name = "Button",)
+
+        #gd = self.add(npyscreen.SimpleGrid, relx = 42, rely=15, width=20)
+        gd = self.add(npyscreen.GridColTitles, relx = 42, rely=15, width=20, col_titles = ['1','2','3','4'])
+        gd.values = []
+        for x in range(36):
+            row = []
+            for y in range(x, x+36):
+                row.append(y)
+            gd.values.append(row)
+
+
+    # This lets the user play with the Form.
+    def while_editing(self, *args, **keywords):
+        self.f.value = self.ch
+
+class TestApp(npyscreen.NPSAppManaged):
+    def onStart(self):
         # These lines create the form and populate it with widgets.
         # A fairly complex screen in only 8 or so lines of code - a line for each control.
-        F =npyscreen.Form(name="Welcome to Npyscreen",)
-        t =F.add(npyscreen.TitleText, name="Textadasdasdasdasdasdasdasd:",)
-        fn=F.add(npyscreen.TitleFilename, name="Filename:")
-        fn2=F.add(npyscreen.TitleFilenameCombo, name="Filename2:")
-        dt=F.add(npyscreen.TitleDateCombo, name="Date:")
-        s =F.add(npyscreen.TitleSlider, out_of=12, name="Slider")
-        ml=F.add(npyscreen.MultiLineEdit,
-               value="""try typing here!\nMutiline text, press ^R to reformat.\n""",
-               max_height=5, rely=9)
-        ms=F.add(npyscreen.TitleSelectOne, max_height=4, value=[1,], name="Pick One",
-                values=["Option1","Option2","Option3"], scroll_exit=True)
-        ms2= F.add(npyscreen.TitleMultiSelect, max_height =-2, value=[1,], name="Pick Several",
-                values=["Option1","Option2","Option3"], scroll_exit=True)
+        F = MyMainFormTest(name = "Welcome to Npyscreen",)
+        self.registerForm("MAIN", F)
 
-        # This lets the user interact with the Form.
+class TestAppz(npyscreen.NPSApp):
+    def main(self):
+        Options = npyscreen.OptionList()
+
+        # just for convenience so we don't have to keep writing Options.options
+        options = Options.options
+
+        options.append(npyscreen.OptionFreeText('FreeText', value='', documentation="This is some documentation."))
+        options.append(npyscreen.OptionMultiChoice('Multichoice', choices=['Choice 1', 'Choice 2', 'Choice 3']))
+        options.append(npyscreen.OptionFilename('Filename', ))
+        options.append(npyscreen.OptionDate('Date', ))
+        options.append(npyscreen.OptionMultiFreeText('Multiline Text', value=''))
+        options.append(npyscreen.OptionMultiFreeList('Multiline List'))
+
+        try:
+            Options.reload_from_file('/tmp/test')
+        except FileNotFoundError:
+            pass
+
+        F  = npyscreen.Form(name = "Welcome to Npyscreen",)
+
+        ms = F.add(npyscreen.OptionListDisplay, name="Option List",
+                values = options,
+                scroll_exit=True,
+                max_height=None)
+
         F.edit()
 
+        Options.write_to_file('/tmp/test')
 
 if __name__ == "__main__":
-    sys.stdout=open('stdout.log', 'w', 1)
-
-    try:
-        App=TestApp2()
+    with open('stdout.log', 'w', 1) as fd:
+        sys.stdout=fd
+        App=theCoreConfiguratorApp()
         App.run()
-    except Exception as e:
-        print(e)
-        sys.stdout.close()
 
